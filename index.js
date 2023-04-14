7c32a89fb2f0992e86d2886825866eca2d86b789
jy7196 <114097609+jy7196@users.noreply.github.com>
2023-04-14 11:23:08 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 
-document.getElementById("togglebutton").addEventListener("click", function(){
-    var button = document.getElementById("togglebutton");
+document.getElementById("toggleeducation").addEventListener("click", function(){
+    var button = document.getElementById("toggleeducation");
     if(button.innerHTML === "Experience"){
-        button.innerHTML = "Education";
+        //button.innerHTML = "Education";
+        document.getElementById("experience").className = "show";
+        document.getElementById("education").className = "hide";
+        button.style.backgroundColor="rgb(26, 24, 44)"
+        document.getElementById("toggleexperience").style.backgroundColor="rgb(51, 48, 73)";
+
+    }
+else{        //button.innerHTML = "Experience";
+        document.getElementById("education").className = "show";
+        document.getElementById("experience").className = "hide";
+        button.style.backgroundColor="rgb(26, 24, 44)"
+        document.getElementById("toggleexperience").style.backgroundColor="rgb(51, 48, 73)";
+    }
+})
+document.getElementById("toggleexperience").addEventListener("click", function(){
+    var button = document.getElementById("toggleexperience");
+    if(button.innerHTML === "Experience"){
+        //button.innerHTML = "Education";
+        button.style.backgroundColor="rgb(26, 24, 44)"
+        document.getElementById("toggleeducation").style.backgroundColor="rgb(51, 48, 73)";
+
         document.getElementById("experience").className = "show";
         document.getElementById("education").className = "hide";
 
     }
-else{        button.innerHTML = "Experience";
+else{       // button.innerHTML = "Experience";
         document.getElementById("education").className = "show";
         document.getElementById("experience").className = "hide";
 
